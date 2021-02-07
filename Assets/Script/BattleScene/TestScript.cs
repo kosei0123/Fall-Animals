6e32c093023495a2f020f8e415646f3ba90c21b3
K.Murakami <mkphysics02@gmail.com>
2021-02-07 11:52:00 +0900

    testscript作成。コミットテスト

---
+++ Assets/Script/BattleScene/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //これはただのテスト。消してOK
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
