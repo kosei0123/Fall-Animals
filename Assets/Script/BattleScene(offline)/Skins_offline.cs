0a8effab0f9ca8242e35f6f587a0c1caf2628ded
kosei0123 <kosei.19970123@i.softbank.jp>
2021-04-09 15:30:17 +0900

    特になし

---
+++ Assets/Script/BattleScene(offline)/Skins_offline.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skins_offline : MonoBehaviour
+{
+    //CharacterMainMove_offlineのpublic定数を使う
+    CharacterMainMove_offline characterMainMove_Offline;
+
+    //Candy
+    [SerializeField]
+    private GameObject Candy;
+    private bool CandyFlag = false;
+    [SerializeField]
+    private GameObject Candy_Sit;
+    private bool Candy_SitFlag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //CharacterMainMove_offlineのpublic定数を使う
+        characterMainMove_Offline = this.gameObject.GetComponent<CharacterMainMove_offline>();
+
+        //None
+        if (SelectSkins.skinsName == null)
+        {
+            Candy.SetActive(false);
+            return;
+        }
+
+        //Candy
+        CandyFlag = (SelectSkins.skinsName == "Candy") ? true : false;
+        Candy.SetActive(CandyFlag);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        //自分の画面の自キャラのみ操作できるようにする
+        if (characterMainMove_Offline.offlineflag == false)
+        {
+            return;
+        }
+
+        //None
+        if (SelectSkins.skinsName == null)
+        {
+            return;
+        }
+
+        //Candy
+        CandyFlag = ((SelectSkins.skinsName == "Candy") && (characterMainMove_Offline.sitFlag == false)) ? true : false;
+        Candy.SetActive(CandyFlag);
+        Candy_SitFlag = ((SelectSkins.skinsName == "Candy") && (characterMainMove_Offline.sitFlag == true)) ? true : false;
+        Candy_Sit.SetActive(Candy_SitFlag);
+
+        //向きの反転
+        //Candy
+        if (((characterMainMove_Offline.transformCache.localScale.z < 0 && characterMainMove_Offline.moveDirection > 0.1) ||
+            (characterMainMove_Offline.transformCache.localScale.z > 0 && characterMainMove_Offline.moveDirection < -0.1)) && Candy.activeSelf == true)
+        {
+            Candy.transform.localScale = new Vector3(Candy.transform.localScale.x, Candy.transform.localScale.y, Candy.transform.localScale.z * (-1));
+        }
+        if (((characterMainMove_Offline.transformCache.localScale.z < 0 && characterMainMove_Offline.moveDirection > 0.1) ||
+            (characterMainMove_Offline.transformCache.localScale.z > 0 && characterMainMove_Offline.moveDirection < -0.1)) && Candy_Sit.activeSelf == true)
+        {
+            Candy_Sit.transform.localScale = new Vector3(Candy_Sit.transform.localScale.x, Candy_Sit.transform.localScale.y, Candy_Sit.transform.localScale.z * (-1));
+        }
+
+    }
+}
---
+++ Assets/Script/Unlock/UnlockSkinsUI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UnlockSkinsUI : MonoBehaviour
+{
+    //SoundManagerのスクリプトの関数使用
+    SoundManager soundManager;
+    //UnlockUIのスクリプトの関数使用
+    UnlockUI unlock;
+
+    //キャンディ
+    //購入ボタン
+    [SerializeField]
+    private Button CandyBuyButton;
+    //値段テキスト
+    [SerializeField]
+    private Text CandyBuyText;
+    //購入完了パネル
+    [SerializeField]
+    private GameObject CandyBuyDonePanel;
+
+    //BuyPanel
+    [SerializeField]
+    private GameObject BuyPanel;
+
+    //値段
+    //キャンディ
+    [HideInInspector]
+    public int candyPrice = 300;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SoundManagerのスクリプトの関数使用
+        soundManager = GameObject.Find("Sound").GetComponent<SoundManager>();
+        //UnlockUIのスクリプトの関数使用
+        unlock = this.gameObject.GetComponent<UnlockUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Buyボタンの押下可能条件
+        CheckBuy();
+        //表示値段の取得
+        CheckTextPrice();
+        //動物購入済みかを確認
+        CheckBuyDone();
+    }
+
+    //Buyボタンの押下可能条件
+    private void CheckBuy()
+    {
+        //キャンディ
+        if (PlayerPrefs.GetInt("myCoin") >= candyPrice && PlayerPrefs.GetInt("Unlock_Candy") == 0) CandyBuyButton.interactable = true;
+        else { CandyBuyButton.interactable = false; }
+    }
+
+    //表示値段の取得
+    private void CheckTextPrice()
+    {
+        //キャンディ
+        CandyBuyText.text = candyPrice.ToString("");
+    }
+
+    //購入済みかを確認
+    private void CheckBuyDone()
+    {
+        //キャンディ
+        if (PlayerPrefs.GetInt("Unlock_Candy") == 1) CandyBuyDonePanel.SetActive(true);
+        else { CandyBuyDonePanel.SetActive(false); }
+    }
+
+    //CandyBuyButtonボタンを押した際の挙動
+    public void OnClick_CandyBuyButton()
+    {
+        //SEの使用
+        soundManager.SEManager("Button_sound1");
+        //キャンディを指定する
+        unlock.unlockName = "Candy";
+        //BuyPanelを表示
+        BuyPanel.SetActive(true);
+    }
+}
